1a744025e0f52c78602f5ae6e6568b8c45a7bf33
Balazs-Rigo <rigo.balazs86@gmail.com>
2024-10-25 16:52:44 +0200

    arrays and statuses

--- react-intro-kanban/src/components/TaskCard.tsx
+++ react-intro-kanban/src/components/TaskCard.tsx
@@ -1,14 +1,14 @@
 import Task from "../utils/data-tasks";
 
-const TaskCard = ({task}: {
-  task:Task  
+const TaskCard = ({ task }: {
+  task: Task
 }) => {
-    return  <div className="border rounded-lg px-2 m-2 bg-gray-100">
-    <div className='text-base font-semibold py-2'>{task.title}</div> 
-    <div className='flex gap-4 justify-between py-2 text-gray-700'>
+  return <div className="border rounded-lg px-2 m-2 bg-gray-100 w-52">
+    <div className='text-base font-base py-2'>{task.title}</div>
+    <div className='flex gap-4 justify-between py-2 text-gray-700 text-sm'>
       <div>{task.id}</div>
       <div>{task.points}</div>
-      </div>
+    </div>
   </div>
 };
 
--- react-intro-kanban/src/utils/data-tasks.ts
+++ react-intro-kanban/src/utils/data-tasks.ts
@@ -1,28 +1,36 @@
+export type Status = 'todo' | 'in-progress' | 'done'
 export type Task ={
     title: string,
     id:string,
+    status: Status,
     points?: number
   }
+
+  export const statuses: Status[] = ['todo', 'in-progress', 'done']
 
   export const tasks: Task[] = [
     {
       title: 'Do Market Reasearch',
       id: 'BUS-1',
+      status: 'todo',
       points: 5,
     },
     {
       title: 'Custom Analysis',
       id: 'BUS-2',
+      status: 'done',
       points: 2,
     },
     {
       title: 'Develop Business strategy',
       id: 'BUS-11',
+      status: 'in-progress',
       points: 8,
     },
     {
       title: 'Develop Market strategy',
       id: 'BUS-1',
+      status: 'in-progress',
       points: 5,
     }
   ]
