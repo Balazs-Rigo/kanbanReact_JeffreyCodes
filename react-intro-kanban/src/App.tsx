1b9c07967a1a452afdca6e59e290498b6d591c67
Balazs-Rigo <rigo.balazs86@gmail.com>
2024-10-25 16:19:48 +0200

    arrays

--- react-intro-kanban/src/App.tsx
+++ react-intro-kanban/src/App.tsx
@@ -1,22 +1,12 @@
 import './App.css'
 import TaskCard from './components/TaskCard'
-import Task from './utils/data-tasks'
+import { tasks } from './utils/data-tasks'
 
 function App() {
-  const task: Task ={
-    title: 'Develop Business Strategy',
-    id: 'BUS-3',
-    points: 5
-  }
 
-  return (    
+  return (
     <>
-      <TaskCard task = {task}></TaskCard>
-      {/* <TaskCard title='Develop Business Strategy?' id='BUS-666' points='4' ></TaskCard>
-      <TaskCard title='Develop Marketing Strategy?' id='BUS-666'></TaskCard> 
-       {card('Competitor Analyst', 'BUS-2', 3)}
-      {card('Develop Business Strategy', 'BUS-3', 5)}
-      {card('Develop Marketing Strategy', 'BUS-4', 7)} */}
+      {tasks.map((task) => <TaskCard task={task} />)}
     </>
 
   )
--- react-intro-kanban/src/utils/data-tasks.ts
+++ react-intro-kanban/src/utils/data-tasks.ts
@@ -4,4 +4,27 @@ export type Task ={
     points?: number
   }
 
+  export const tasks: Task[] = [
+    {
+      title: 'Do Market Reasearch',
+      id: 'BUS-1',
+      points: 5,
+    },
+    {
+      title: 'Custom Analysis',
+      id: 'BUS-2',
+      points: 2,
+    },
+    {
+      title: 'Develop Business strategy',
+      id: 'BUS-11',
+      points: 8,
+    },
+    {
+      title: 'Develop Market strategy',
+      id: 'BUS-1',
+      points: 5,
+    }
+  ]
+
   export default Task
