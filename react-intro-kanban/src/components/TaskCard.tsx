4ca0eef260d48408eb0a0258ae6a9b7dbf86e6d1
Balazs-Rigo <rigo.balazs86@gmail.com>
2024-10-25 22:18:05 +0200

    priority icons

--- react-intro-kanban/src/components/TaskCard.tsx
+++ react-intro-kanban/src/components/TaskCard.tsx
@@ -1,12 +1,19 @@
 import Task from "../utils/data-tasks";
 
+const lowPriorityIcon = <svg version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 122.88 80.593" enable-background="new 0 0 122.88 80.593"><g><polygon points="122.88,0 122.88,30.82 61.44,80.593 0,30.82 0,0 61.44,49.772 122.88,0" /></g></svg>
+
 const TaskCard = ({ task }: {
   task: Task
 }) => {
   return <div className="border rounded-lg px-2 m-2 bg-gray-100 w-52">
     <div className='text-base font-base py-2'>{task.title}</div>
     <div className='flex gap-4 justify-between py-2 text-gray-700 text-sm'>
-      <div>{task.id}</div>
+      <div className="flex gap-2">
+        <div>{task.id}</div>
+        {task.priority === 'high' && lowPriorityIcon}
+        {task.priority === 'medium' && lowPriorityIcon}
+        {task.priority === 'low' && lowPriorityIcon}
+      </div>
       <div>{task.points}</div>
     </div>
   </div>
--- react-intro-kanban/src/utils/data-tasks.ts
+++ react-intro-kanban/src/utils/data-tasks.ts
@@ -1,12 +1,15 @@
 export type Status = 'todo' | 'in-progress' | 'done'
+export type Priority = 'low' |'medium' | 'high'
 export type Task ={
     title: string,
     id:string,
     status: Status,
+    priority: Priority
     points?: number
   }
 
   export const statuses: Status[] = ['todo', 'in-progress', 'done']
+  export const priorities: Priority[] = ['low', 'medium', 'high']
 
   export const tasks: Task[] = [
     {
@@ -14,24 +17,28 @@ export type Task ={
       id: 'BUS-1',
       status: 'todo',
       points: 5,
+      priority: 'high'
     },
     {
       title: 'Custom Analysis',
       id: 'BUS-2',
       status: 'done',
       points: 2,
+      priority: 'medium'
     },
     {
       title: 'Develop Business strategy',
       id: 'BUS-11',
       status: 'in-progress',
       points: 8,
+      priority: 'low'
     },
     {
       title: 'Develop Market strategy',
       id: 'BUS-1',
       status: 'in-progress',
       points: 5,
+      priority: 'low'
     }
   ]
 
