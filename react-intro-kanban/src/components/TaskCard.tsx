2386606fadce9761b519ab66efbe0cb28201e147
Balazs-Rigo <rigo.balazs86@gmail.com>
2024-10-26 19:02:21 +0200

    update task points

--- react-intro-kanban/src/components/TaskCard.tsx
+++ react-intro-kanban/src/components/TaskCard.tsx
@@ -1,20 +1,36 @@
+import { useState } from "react";
 import Task from "../utils/data-tasks";
 
-const lowPriorityIcon = <svg version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 122.88 80.593" enable-background="new 0 0 122.88 80.593"><g><polygon points="122.88,0 122.88,30.82 61.44,80.593 0,30.82 0,0 61.44,49.772 122.88,0" /></g></svg>
+const lowPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 9l7 7 7-7" />
+</svg>
+const mediumPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10h14" />
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 14h14" />
+</svg>
+const highPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
+</svg>
 
-const TaskCard = ({ task }: {
+const TaskCard = ({ task, updateTaskPoints }: {
   task: Task
+  updateTaskPoints: (task: Task, points: number) => void
 }) => {
+  const points = task.points || 0;
   return <div className="border rounded-lg px-2 m-2 bg-gray-100 w-52">
     <div className='text-base font-base py-2'>{task.title}</div>
     <div className='flex gap-4 justify-between py-2 text-gray-700 text-sm'>
       <div className="flex gap-2">
         <div>{task.id}</div>
-        {task.priority === 'high' && lowPriorityIcon}
-        {task.priority === 'medium' && lowPriorityIcon}
+        {task.priority === 'high' && highPriorityIcon}
+        {task.priority === 'medium' && mediumPriorityIcon}
         {task.priority === 'low' && lowPriorityIcon}
       </div>
-      <div>{task.points}</div>
+      <div className="flex gap-2 items-center">
+        <button onClick={() => updateTaskPoints(task, points - 1)}>-</button>
+        <div className="font-bold">{points}</div>
+        <button onClick={() => updateTaskPoints(task, points + 1)}>+</button>
+      </div>
     </div>
   </div>
 };
--- react-intro-kanban/src/utils/data-tasks.ts
+++ react-intro-kanban/src/utils/data-tasks.ts
@@ -1,45 +1,45 @@
 export type Status = 'todo' | 'in-progress' | 'done'
-export type Priority = 'low' |'medium' | 'high'
-export type Task ={
-    title: string,
-    id:string,
-    status: Status,
-    priority: Priority
-    points?: number
+export type Priority = 'low' | 'medium' | 'high'
+export type Task = {
+  title: string,
+  id: string,
+  status: Status,
+  priority: Priority
+  points?: number
+}
+
+export const statuses: Status[] = ['todo', 'in-progress', 'done']
+export const priorities: Priority[] = ['low', 'medium', 'high']
+
+export const tasks: Task[] = [
+  {
+    title: 'Do Market Reasearch',
+    id: 'BUS-1',
+    status: 'todo',
+    points: 5,
+    priority: 'high'
+  },
+  {
+    title: 'Custom Analysis',
+    id: 'BUS-2',
+    status: 'done',
+    points: 2,
+    priority: 'medium'
+  },
+  {
+    title: 'Develop Business strategy',
+    id: 'BUS-11',
+    status: 'in-progress',
+    points: 8,
+    priority: 'low'
+  },
+  {
+    title: 'Develop Market strategy',
+    id: 'BUS-14',
+    status: 'in-progress',
+    points: 5,
+    priority: 'low'
   }
+]
 
-  export const statuses: Status[] = ['todo', 'in-progress', 'done']
-  export const priorities: Priority[] = ['low', 'medium', 'high']
-
-  export const tasks: Task[] = [
-    {
-      title: 'Do Market Reasearch',
-      id: 'BUS-1',
-      status: 'todo',
-      points: 5,
-      priority: 'high'
-    },
-    {
-      title: 'Custom Analysis',
-      id: 'BUS-2',
-      status: 'done',
-      points: 2,
-      priority: 'medium'
-    },
-    {
-      title: 'Develop Business strategy',
-      id: 'BUS-11',
-      status: 'in-progress',
-      points: 8,
-      priority: 'low'
-    },
-    {
-      title: 'Develop Market strategy',
-      id: 'BUS-1',
-      status: 'in-progress',
-      points: 5,
-      priority: 'low'
-    }
-  ]
-
-  export default Task
+export default Task
